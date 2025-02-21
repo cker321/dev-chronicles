a7ce9feb16e2adcb4f66a4781f84966c751cc1ab
朱瑞 <zhurui@cloudwalk.com>
2025-02-21 16:05:04 +0800

    feat: add ico

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -28,7 +28,8 @@ export default defineConfig({
               collapsed: false,
               items: [
                 { text: '概述', link: '/development/frontend/' },
-                { text: 'Nginx Vue Router 经验', link: '/development/frontend/nginx-vue-router-experience' }
+                { text: 'Nginx Vue Router 经验', link: '/development/frontend/nginx-vue-router-experience' },
+                { text: 'Vue Rollup Practice 实践', link: '/development/frontend/vue-rollup-practice' }
               ]
             },
             { text: '后端', link: '/development/backend/' },
